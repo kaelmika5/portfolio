8f286c6142ec22150f1242664da9fd743e08a4aa
kaelmika5 <kaelmika5@gmail.com>
2023-04-29 01:30:15 +0300

    img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
-
+import kael from '../images/kael.png'
 import cv from '../images/cv-kael.pdf'
 
 function Home() {
     return (
         <section className="home-content">
             <div id="home" className="container">
-                <h1>Intégrateur Graphiste<br /> Sénior</h1>
-                <a className="btn btn-custom" href={cv} target="_blank" rel="noreferrer">Télécharger mon CV</a>
+                <div className="row">
+                    <div className="col-md-5">
+                        <img className='me-img' src={kael} alt="kael"/>
+                    </div>
+                    <div className="col-md-7">
+                        <h1>Intégrateur web<br /> Developpeur front-end</h1>
+                        <a className="btn btn-custom" href={cv} target="_blank" rel="noreferrer">Télécharger mon CV</a>
+                    </div>
+                </div>
             </div>
         </section>
     )
